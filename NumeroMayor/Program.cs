27caee88aeb1ce197dae762ee583f89ab8306540
EderDev <eder83x@gmail.com>
2024-09-14 15:05:08 -0500

    Ejercicio02 - Numero Mayor.

---
+++ NumeroMayor/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ejercicio02;
+
+class Program
+{
+    static void Main (string[] args) 
+    {
+        //variables del programa
+        int a, b, c;
+        do
+        {
+            Console.WriteLine("Ingrese primer numero: ");
+            a =  int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            b = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Ingrese tercer numero :");
+            c = int.Parse(Console.ReadLine()!);
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"El numero mayor es: {a}.");
+            }
+            else
+            {
+                if (b > c)
+                {
+                    Console.WriteLine($"El numero mayor es: {b}.");
+                }
+                else
+                {
+                    Console.WriteLine($"El numero mayor es: {c}.");
+                }
+            }
+
+           
+        } while (true);
+    
+    }
+}
